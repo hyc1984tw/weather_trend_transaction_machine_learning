ac11e2d4f79a6a8b2cc997d6d88dfc0dc6372955
Y.H. Chen <michaelyenoke@hotmail.com>
2020-11-20 02:41:18 +0800

    Create 04_national_holiday_transfer.sh

---
+++ 03_bigquery/04_national_holiday_transfer.sh
@@ -0,0 +1,10 @@
+# 2017
+
+bq --location=US load \
+--field_delimiter=';' \
+--source_format=CSV \
+--skip_leading_rows=1 \
+tw_holiday.nationalholiday_2017_2020 \
+gs://tw_national_holiday_2017_2020/tw_national_holiday/2017.csv \
+year_weeknum:STRING,weekday:STRING,holiday:STRING,note:STRING
+
