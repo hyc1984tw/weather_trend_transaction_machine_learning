b3e230a53b1cfc4e05ea405fb1eb28e5e0524ba8
Y.H. Chen <michaelyenoke@hotmail.com>
2020-11-19 14:19:42 +0800

    Update 02_transfer_from_storage.sh

--- 03_bigquery/02_transfer_from_storage.sh
+++ 03_bigquery/02_transfer_from_storage.sh
@@ -3,7 +3,7 @@
 
 bq --location=US load \
 --source_format=CSV \
-tw_weather.467490_2016_07 \
+tw_weather.taichung_201607_202010 \
 gs://tw_weather_taichung_201607_202010/forcast_station_weather/467490-2016-07.csv \
 
 
