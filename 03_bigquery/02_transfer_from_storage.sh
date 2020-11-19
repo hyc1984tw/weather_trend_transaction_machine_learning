188fdafbbe8ddf4deee614598293d8f094f45fb9
Y.H. Chen <michaelyenoke@hotmail.com>
2020-11-19 11:00:35 +0800

    Update 02_transfer_from_storage.sh

--- 03_bigquery/02_transfer_from_storage.sh
+++ 03_bigquery/02_transfer_from_storage.sh
@@ -1,4 +1,7 @@
 # transfer data download from https://e-service.cwb.gov.tw/HistoryDataQuery/index.jsp to bigquery to create table
 
 
-bq --location=US load \ --source_format=CSV \ tw_weather.467490_2016_07 \ gs://tw_weather_taichung_201607_202010/forcast_station_weather/467490-2016-07.csv \ --autodetect
+bq --location=US load \
+--source_format=CSV \
+tw_weather.467490_2016_07 \
+gs://tw_weather_taichung_201607_202010/forcast_station_weather/467490-2016-07.csv \
